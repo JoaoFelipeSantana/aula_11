a63b3572810bf5272465e827749e458823929cd9
JoaoFelipeSantana <145509170+JoaoFelipeSantana@users.noreply.github.com>
2024-05-17 17:39:33 -0300

    [feat] - Adicionando os atributos e encapsulando a classe "Empregado"

--- aula_11/src/Empregado.java
+++ aula_11/src/Empregado.java
@@ -1,3 +1,46 @@
 public class Empregado {
     
+    protected String name;
+    protected String address;
+    protected double salary;
+
+    // Contrutor
+    public Empregado() {
+
+    }
+
+    public Empregado(String newName, String newAddress) {
+        this.name = newName;
+        this.address = newAddress;
+    }
+
+    // Métodos acessores
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    // Métodos modificadores
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public void setAddress(String newAddress) {
+        this.address = newAddress;
+    }
+
+    public void setSalary(double newSalary) {
+        this.salary = newSalary;
+    }
+
+    
+    
+
 }
