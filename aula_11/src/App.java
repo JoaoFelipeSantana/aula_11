95aa837ed9ca21d03897571db5dc61659b33af91
JoaoFelipeSantana <145509170+JoaoFelipeSantana@users.noreply.github.com>
2024-05-20 16:54:29 -0300

    [feat] - Criando e mostrando as informações dos objetos "Mensalista"

--- aula_11/src/App.java
+++ aula_11/src/App.java
@@ -1,5 +1,30 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Mensalista men1, men2;
+        Horista hora1, hora2;
+
+        // Empregados Mensalistas
+        // men1
+        men1 = new Mensalista("Jose", "Rua abc", "Junior");
+        men1.calcularSalario();
+        men1.calcularInss();
+
+        System.out.println("==============================");
+        System.out.println("Nome: " + men1.getName());
+        System.out.println("Endereço: " + men1.getAddress());
+        System.out.printf("Salario: %.2f\n", men1.getSalary());
+        System.out.println("==============================");
+
+
+        // men2
+        men2 = new Mensalista("Ana", "Rua sem fim", "Senior");
+        men2.calcularSalario();
+        men2.calcularInss();
+
+        System.out.println("==============================");
+        System.out.println("Nome: " + men2.getName());
+        System.out.println("Endereço: " + men2.getAddress());
+        System.out.printf("Salario: %.2f\n", men2.getSalary());
+        System.out.println("==============================");
     }
 }
