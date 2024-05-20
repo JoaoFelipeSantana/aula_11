ccdd97a00fe6c271cdeed7b880624d9fe7153696
JoaoFelipeSantana <145509170+JoaoFelipeSantana@users.noreply.github.com>
2024-05-20 16:29:11 -0300

    [feat] - Adicionando os contrutores e os métodos modificador e acessor

--- aula_11/src/Mensalista.java
+++ aula_11/src/Mensalista.java
@@ -1,3 +1,24 @@
-public class Mensalista {
+public class Mensalista extends Empregado{
     
+    private String position;
+
+    // Construtores
+    public Mensalista(String newName, String newAddress, String newPosition) {
+        super(newName, newAddress);
+        this.position = newPosition;
+    }
+
+    public Mensalista() {
+
+    }
+
+    // Método Acessore
+    public String getPosition() {
+        return position;
+    }
+
+    // Método Modificador
+    public void setPosition(String newPosition) {
+        this.position = newPosition;
+    }
 }
