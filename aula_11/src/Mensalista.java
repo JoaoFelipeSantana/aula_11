35ab405c0bdf211ab8085a95f658123eb774658b
JoaoFelipeSantana <145509170+JoaoFelipeSantana@users.noreply.github.com>
2024-05-20 16:37:16 -0300

    [feat] - Adicionando o método "calcularSalario"

--- aula_11/src/Mensalista.java
+++ aula_11/src/Mensalista.java
@@ -21,4 +21,16 @@ public class Mensalista extends Empregado{
     public void setPosition(String newPosition) {
         this.position = newPosition;
     }
+
+    public void calcularSalario() {
+        if (position.toUpperCase().equals("JUNIOR")){
+            salary = 3658.92;
+        }
+        else if(position.toUpperCase().equals("PLENO")) {
+            salary = 7387.42;
+        }
+        else if (position.toUpperCase().equals("SENIOR")) {
+            salary = 14399.46;
+        }
+    }
 }
